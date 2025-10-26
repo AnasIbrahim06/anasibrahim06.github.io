0dff1e039c2fa48f52061ad0709eeec5f0cbd6eb
Anas Ibrahim <anasibru3@gmail.com>
2025-10-26 12:05:20 +0530

    Added projects

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -12,19 +11,4 @@ def projects():
     return render_template("projects.html")
 
 if __name__ == "__main__":
-=======
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
-
-if __name__ == "__main__":
->>>>>>> 9d79de27db77a1c472c4358bdfb2b37ace8509d7
     app.run(debug=True)
