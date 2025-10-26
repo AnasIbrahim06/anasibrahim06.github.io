b0d273b30e76d5204a506d3cdeb47976b766c90d
Anas Ibrahim <anasibru3@gmail.com>
2025-10-26 15:01:26 +0530

    Added projects4

--- main.py
+++ main.py
@@ -10,5 +10,9 @@ def home():
 def projects():
     return render_template("projects.html")
 
+@app.route("/projectsdetails1")
+def projectsdetails1():
+    return render_template("projectdetails1.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
